487c1eec2eacd0c8bc9313b8e903fc3147822a4b
Grant Hendricks <136547087+gnhen@users.noreply.github.com>
2026-06-24 09:32:02 -0400

    Delete inject.js

--- inject.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    if (window.kendo && window.jQuery) {
-        var grid = window.jQuery("#Sales").data("kendoGrid");
-        if (grid) {
-            grid.dataSource.pageSize(300);
-        }
-    }
-})();
